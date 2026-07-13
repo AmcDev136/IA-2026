692db0e02a5127fcddd65ee004255178ae9057ff
Amilcar136 <amilcar13698@gmail.com>
2026-07-13 14:58:09 -0600

    Fix: Recorte de rostro preciso

--- haar_cascade.py
+++ haar_cascade.py
@@ -5,26 +5,33 @@ rostro = cv.CascadeClassifier('haarcascade_frontalface_alt2.xml')
 cap = cv.VideoCapture(0)
 x=y=w=h= 0 
 count = 0
-img = 0
+img = None
 while True:
     ret, frame = cap.read()
+    if not ret:
+        break
+
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     rostros = rostro.detectMultiScale(gray, 1.3, 5)
     for(x, y, w, h) in rostros:
+        img = frame[y:y+h, x:x+w] # recorte del rostro
+        count = count + 1
+        name = 'D:/IA Verano 2026/Caras/Cara'+str(count)+'.jpg'
+        cv.imwrite(name, img)
+
         m1 = int(h/2)
         n1 = int(w/2)
         frame = cv.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
         frame = cv.circle(frame, (x+n1,y+m1), int(w/2) , (255, 0 ,0), 2 )
-        img = frame[y:y+h,x:x+w]
-        count = count + 1   
 
-    name = 'D:\\IA Verano 2026\\Caras\\'+str(count)+'.jpg'
-    cv.imwrite(name, frame)
     cv.imshow('rostros', frame)
-    cv.imshow('cara', img)
+
+    if img is not None:
+        cv.imshow('cara', img)
     
     k = cv.waitKey(1)
     if k == 27:
         break
+
 cap.release()
 cv.destroyAllWindows()
